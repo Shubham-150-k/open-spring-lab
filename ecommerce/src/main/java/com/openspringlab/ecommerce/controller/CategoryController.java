f314484fca9bc64b33eecce41e265dd09b4c9d16
Shubham-150-k <kumbhareshubham33@gmail.com>
2025-12-27 01:41:02 +0530

    Category Service class with createCategoty and getActiveCategories method

---
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/controller/CategoryController.java
@@ -0,0 +1,4 @@
+package com.openspringlab.ecommerce.controller;
+
+public class CategoryController {
+}
---
+++ ecommerce/src/main/java/com/openspringlab/ecommerce/product/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.openspringlab.ecommerce.product;
+
+public interface CategoryRepository {
+}
